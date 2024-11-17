533cb68370d98ef7a95626ddcb35e2406af1b091
sjh7711 <apfhd159862@gmail.com>
2024-11-18 06:51:04 +0900

    server ip 표기

--- photoshare/photos/views.py
+++ photoshare/photos/views.py
@@ -186,7 +186,14 @@ def get_photo_data(request):
     
     return JsonResponse(data)
 
+import socket
+def get_server_ip():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
 def photo_list(request):
+    server_ip = get_server_ip()
     sort = request.GET.get('sort', 'latest')
     query_description = request.GET.get('query_description', '')
     query_username = request.GET.get('query_username', '')
@@ -273,7 +280,8 @@ def photo_list(request):
         'search_form': search_form,
         'uploaders': uploaders,
         'active_users': active_users,
-        'notifications': notifications
+        'notifications': notifications,
+        'server_ip': server_ip
     })
 
 @login_required
