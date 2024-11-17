256df9c802f0ca47bf758d546e72587c08d60ae3
sjh7711 <apfhd159862@gmail.com>
2024-11-17 14:49:56 +0900

    allowed host 수정

--- photoshare/config/settings.py
+++ photoshare/config/settings.py
@@ -92,9 +92,10 @@ LOGGING = {
 }
 
 from socket import gethostname, gethostbyname_ex
-ALLOWED_HOSTS = [ gethostname(), ] + list(set(gethostbyname_ex(gethostname())[2]))
+ALLOWED_HOSTS = [ gethostname(), ] + list(set(gethostbyname_ex(gethostname())[2])) + ['192.168.0.210', '192.168.0.211', '192.168.0.212', '192.168.0.213']
 
-CSRF_TRUSTED_ORIGINS = ['http://10.10.{}.{}'.format(i,j) for i in range(256) for j in range(256)]
+CSRF_TRUSTED_ORIGINS = ['http://10.10.{}.{}'.format(i,j) for i in range(256) for j in range(256)] + ['192.168.0.210', '192.168.0.211', '192.168.0.212', '192.168.0.213']
+
 
 # Application definition
 
